2c0989ce933c79fa4696d00489f9ba211cd868bc
Vitalii-Dzoma <vitoos900@gmail.com>
2021-12-06 16:36:39 +0200

    финал

--- src/index.js
+++ src/index.js
@@ -2,26 +2,30 @@
 import API from "./fetchImages.js"
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { pageNum } from "./fetchImages.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import simpleLightbox from "simplelightbox";
 
 
+let count = 1;
+let gallery = new SimpleLightbox('.gallery a');
 const inputData = document.querySelector('input');
 const formData = document.querySelector(".search-form");
-const galleryRender = document.querySelector('.gallery')
-const btnLoadMore = document.querySelector('.load-more')
+const galleryRender = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
 console.log(inputData.value)
 btnLoadMore.classList.add('hidden')
 async function onSearch(e) {
     e.preventDefault()
-    console.log(e)
 
 const image = await API.fetchImages(inputData.value)
     console.log(image);
     if (image.hits.length === 0) {
         Notify.info("Sorry, there are no images matching your search query. Please try again.")
     }
-  galleryRender.innerHTML = await image.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+  galleryRender.innerHTML =  await image.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
                   `<div class="photo-card">
-  <img src=${webformatURL} alt="" loading="lazy" width=300px height=150px/>
+  <a class="gallery__item" href="${largeImageURL}"><img src=${webformatURL} alt="" loading="lazy" width=300px height=150px/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -38,16 +42,21 @@ const image = await API.fetchImages(inputData.value)
   </div>
 </div>`
     ).join('')
+
+gallery.on('show.simplelightbox', simpleLightbox);
+
+    Notify.success(`Hooray! We found ${image.totalHits} totalHits images.`)
     btnLoadMore.classList.remove('hidden')
+  
 }
 
 
 async function clickLoadMore() {
-    
- const image = await API.fetchImages(inputData.value)
-    galleryRender.insertAdjacentElement = ('beforeend', await image.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+    count+=1
+    const image = await API.fetchImages(inputData.value, count)
+   const markup = await image.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
                   `<div class="photo-card">
-  <img src=${webformatURL} alt="" loading="lazy" width=300px height=150px/>
+  <a class="gallery__item" href="${largeImageURL}"><img src=${webformatURL} alt="" loading="lazy" width=300px height=150px/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -63,8 +72,29 @@ async function clickLoadMore() {
     </p>
   </div>
 </div>`
-    ).join('')) 
+    ).join('')
+    galleryRender.insertAdjacentHTML("beforeend", markup)
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+    const renderedMarkup = document.querySelectorAll('.photo-card');
+    
+    renderedMarkup.forEach((element, index) => {
+        if (index > image.totalHits) {
+       Notify.info("We're sorry, but you've reached the end of search results.")
+            count = 1;
+            btnLoadMore.classList.add('hidden')
+        }
+gallery.on('show.simplelightbox', simpleLightbox);
+gallery.refresh()
+})
 }
+
+  
 
   export default { clickLoadMore };
 formData.addEventListener("submit", onSearch)
